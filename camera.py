7c2c6ed501ca87cb8bcaa7536a462a6488279bed
ricaellenascimento <ricaellenascimento@gmail.com>
2025-03-25 19:53:32 -0300

    add: código base

---
+++ camera.py
@@ -0,0 +1,37 @@
+import math
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from pygame.locals import *
+
+camera_angle_x = 15.0 
+camera_angle_y = 0.0 
+camera_distance = 6.0 
+
+def setup_camera():
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    gluLookAt(camera_distance * math.sin(math.radians(camera_angle_y)),
+              camera_distance * math.sin(math.radians(camera_angle_x)),
+              camera_distance * math.cos(math.radians(camera_angle_y)),
+              0.0, 1, 0,
+              0.0, 1.0, 0.0)
+
+def handle_camera_movement(keys):
+    global camera_angle_x, camera_angle_y, camera_distance
+
+    camera_speed = 0.5
+    zoom_speed = 0.1
+
+    if keys[K_LEFT]:
+        camera_angle_y -= camera_speed
+    if keys[K_RIGHT]:
+        camera_angle_y += camera_speed
+    if keys[K_UP]:
+        camera_angle_x -= camera_speed
+    if keys[K_DOWN]:
+        camera_angle_x += camera_speed
+    if keys[K_w]:
+        camera_distance -= zoom_speed
+    if keys[K_s]:
+        camera_distance += zoom_speed
---
+++ textures.py
@@ -0,0 +1,16 @@
+from OpenGL.GL import *
+import pygame
+
+def load_texture(filename):
+    texture_surface = pygame.image.load(filename)
+    texture_data = pygame.image.tostring(texture_surface, "RGB", 1)
+    width = texture_surface.get_width()
+    height = texture_surface.get_height()
+
+    texture_id = glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D, texture_id)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, texture_data)
+
+    return texture_id
